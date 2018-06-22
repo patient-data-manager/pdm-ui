39edad471cfe7bf7300dea2c7c4bbc33346c82e3
sgober <sgober@mitre.org>
2018-06-22 08:10:14 -0400

    initial sidebar look and feel

--- src/components/Body/Sidebar.js
+++ src/components/Body/Sidebar.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
 import FontAwesome from 'react-fontawesome';
 
 // Each logical "route" has two components, one for
@@ -8,53 +9,108 @@ import FontAwesome from 'react-fontawesome';
 // path matches the current URL.
 const routes = [
   {
-    path: "/",
-    sidebar: () => <div className="Options"><FontAwesome name="user-circle" />    PROFILES</div>,
+    path: "/profiles",
+    // sidebar: () => <div className="Options"><FontAwesome name="user-circle" />PROFILES</div>,
+    main: () => <h2> INSERT PROFILE PANEL HERE </h2>
   },
   {
-    path: "/",
-    sidebar: () => <div className="Options"><FontAwesome name = "heart" />    HEALTH RECORD</div>,
+    path: "/health_record"
+    // sidebar: () => <div className="Options"><FontAwesome name = "heart" />HEALTH RECORD</div>,
   },
   {
-    path: "/",
-    sidebar: () => <div className="Options"><FontAwesome name="exclamation-circle" />   ALERTS</div>,
+    path: "/alerts"
+    // sidebar: () => <div className="Options"><FontAwesome name="exclamation-circle" />ALERTS</div>,
   },
   {
-    path: "/",
-    sidebar: () => <div className="Options"><FontAwesome name="hospital-o" />   PROVIDERS</div>,
+    path: "/providers"
+    // sidebar: () => <div className="Options"><FontAwesome name="hospital-o" />PROVIDERS</div>,
   }
 ];
 
-const SidebarExample = () => (
-  <Router>
-    <div style={{ display: "flex" }}>
-      <div
-        style={{
-          listStyleType: "none",
-          padding: "100px",
-          width: "70%",
-          background: "#f0f0f0"
-        }}
-      >
+const Sidebar = () => (
 
-        {routes.map((route, index) => (
-          // You can render a <Route> in as many places
-          // as you want in your app. It will render along
-          // with any other <Route>s that also match the URL.
-          // So, a sidebar or breadcrumbs or anything else
-          // that requires you to render multiple things
-          // in multiple places at the same URL is nothing
-          // more than multiple <Route>s.
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.sidebar}
-          />
-        ))}
-      </div>
-    </div>
-  </Router>
+  <div className='sidebar'>
+     <SideNav highlightColor='#fff' highlightBgColor='#6699cc'>
+         <Nav id='profiles'>
+             <NavIcon><FontAwesome name='user-circle' /></NavIcon>
+             <NavText> PROFILES </NavText>
+         </Nav>
+         <Nav id='healthRecord' className='Options'>
+             <NavIcon><FontAwesome name='heart' /></NavIcon>
+             <NavText> HEALTH RECORD </NavText>
+         </Nav>
+         <Nav id='alerts' className='Options'>
+             <NavIcon><FontAwesome name='exclamation-circle' /></NavIcon>
+             <NavText> ALERTS </NavText>
+         </Nav>
+         <Nav id='providers' className='Options'>
+             <NavIcon><FontAwesome name='hospital-o' /></NavIcon>
+             <NavText> PROVIDERS </NavText>
+         </Nav>
+      </SideNav>
+  </div>
+  // <Router>
+  //   <div style={{ display: "flex" }}>
+  //     <div
+  //       style={{
+  //         listStyleType: "none",
+  //         marginTop: "100px",
+  //         marginLeft: "15px",
+  //         padding: "10px",
+  //         width: "100%",
+  //         height: "100%",
+  //         background: "#f0f0f0"
+  //       }}
+  //     >
+  //     <ul style={{ listStyleType: "none", padding: 0 }}>
+  //       <li>
+  //         <div className="Options">
+  //           <Link to="/profiles">
+  //             <FontAwesome name="user-circle" /> PROFILES
+  //           </Link>
+  //         </div>
+  //       </li>
+  //       <li>
+  //         <div className="Options">
+  //           <Link to="/health_record">
+  //             <FontAwesome name="heart" /> HEALTH RECORD
+  //           </Link>
+  //         </div>
+  //       </li>
+  //       <li>
+  //         <div className="Options">
+  //           <Link to="/alerts">
+  //             <FontAwesome name="exclamation-circle" /> ALERTS
+  //           </Link>
+  //         </div>
+  //       </li>
+  //       <li>
+  //         <div className="Options">
+  //           <Link to="/providers">
+  //             <FontAwesome name="hospital-o" /> PROVIDERS
+  //           </Link>
+  //         </div>
+  //       </li>
+  //     </ul>
+  //
+  //       {routes.map((route, index) => (
+  //         // You can render a <Route> in as many places
+  //         // as you want in your app. It will render along
+  //         // with any other <Route>s that also match the URL.
+  //         // So, a sidebar or breadcrumbs or anything else
+  //         // that requires you to render multiple things
+  //         // in multiple places at the same URL is nothing
+  //         // more than multiple <Route>s.
+  //         <Route
+  //           key={index}
+  //           path={route.path}
+  //           exact={route.exact}
+  //           component={route.sidebar}
+  //         />
+  //       ))}
+  //     </div>
+  //   </div>
+  // </Router>
 );
 
-export default SidebarExample;
+export default Sidebar;
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import robot from '../../../images/robot.png'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Logo extends Component {
   render() {
