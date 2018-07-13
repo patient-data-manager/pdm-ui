3e61f9cea755657751826f5154c81a0acc4c3250
Noranda Brown <nebrown@mitre.org>
2018-07-13 00:01:19 -0400

    Refactored providers axios calls to put the access token into the headers

--- src/actions/providers.js
+++ src/actions/providers.js
@@ -3,37 +3,43 @@ import axios from 'axios';
 
 export function fetchProviders() {
   return (dispatch, getState) => {
-    const access_token = getState().auth.accessToken;
+    const accessToken = getState().auth.accessToken;
 
     return dispatch({
       type: types.FETCH_PROVIDERS,
-      payload: axios.get('/api/v1/providers?access_token='+access_token,{},
-       {headers: {'X-Key-Inflection': 'camel',
-          'Accept': 'application/json'}})
+      payload: axios.get(
+        '/api/v1/providers',
+        { headers: { 'X-Key-Inflection': 'camel', Accept: 'application/json', Authorization: `Bearer ${accessToken}` } }
+      )
     });
   }
 }
 
 export function linkProvider(provider_id, profile_id) {
   return (dispatch, getState) => {
-    const access_token = getState().auth.accessToken;
-    const redirect_uri = window.location.protocol + '//' + window.location.host + '/oauth';
-    const data = { access_token, provider_id, redirect_uri };
-    axios.post('/api/v1/profiles/'+profile_id+'/providers', data,
-       {headers: {'X-Key-Inflection': 'camel',
-          'Accept': 'application/json'}}).then((response) => {
-            window.location=response.data.redirect_uri;
-          })
-    }
+    const accessToken = getState().auth.accessToken;
+    const redirect_uri = `${window.location.protocol}//${window.location.host}/oauth`;
+    const data = { provider_id, redirect_uri };
+
+    axios.post(
+      `/api/v1/profiles/${profile_id}/providers`,
+      data,
+      { headers: { 'X-Key-Inflection': 'camel', Accept: 'application/json', Authorization: `Bearer ${accessToken}` } }
+    )
+      .then((response) => {
+        window.location = response.data.redirect_uri;
+      });
+  }
 }
 
 export function oauthCallback(state, code) {
   return (dispatch, getState) => {
     return {
       type: types.PROVIDE_OAUTH_TOKEN,
-      payload: axios.get('/oauth/callback?state=' + state +'&code=' + code, {},
-       {headers: {'X-Key-Inflection': 'camel',
-          'Accept': 'application/json'}})
-    }
+      payload: axios.get(
+        `/oauth/callback?state=${state}&code=${code}`,
+        { headers: { 'X-Key-Inflection': 'camel', Accept: 'application/json' } }
+      )
+    };
   }
 }
