c91342feccd2df4770edec4f8733ddbdcfed1e1e
sgober <sgober@mitre.org>
2018-07-13 09:09:05 -0400

    initial TOC on health record page work

---
+++ src/components/dashboard/health-record/Summary.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+
+export class Summary extends Component {
+    render() {
+        return(
+            <p> Here I am in this strange country about which I have learned in the
+            geography and history, and about which I heard my father talk. The
+            daughter of an American man and a Chinese woman, I suppose I am what is
+            called a mongrel. My father was a Commissioner of Customs in China, and
+            living for years in that country he fell in love with my mother and
+            married her--as was natural. Who could help falling in love with my
+            dear, yellow, winsome, little mother? My name is Margaret, called after
+            my father's mother; my father said that the word Margaret means a pearl,
+            so he gave me the pet name "Pearl." Dear father!
+            
+            "It was a monstrous thing for Brother George to marry away there," I
+            overheard my Aunt Gwendolin remark a short time after my arrival. "Why
+            could he not have come back home to his own country and found a
+            wife?--And above all to have married a heathen Chinese!"
+            
+            "Not a heathen," said my grandmother, reproachfully, "she had previously
+            embraced the faith of Europeans; so my dear George wrote me from that
+            far-away country."
+            
+            "Oh, they are all heathens in my estimation," cried my Aunt Gwendolin,
+            scornfully; "what faith they embrace does not change the fact that they
+            belong to the yellow people."
+            
+            My mother died while I was yet a child, and my father has died and left
+            me alone in the world within the last year. Grandmother, my father's
+            mother, when she learned about her son's death, sent at once for me.
+            
+            "I cannot leave a granddaughter of mine in that country, and among that
+            heathen, if not barbarous, people," she wrote to the American consul,
+            "and I ask your services to assist her to come to my home in America."
+            
+            The consul, absent-minded, gave me my grandmother's letter to read, and
+            thus I learned her feeling about my mother's people and country. I never
+            would have come to this horrible America if I could have helped myself;
+            but I am scarcely of age, and by my father's will grandmother is
+            appointed my guardian.
+            
+            The result of it all is, that having crossed the intervening waters, I
+            am here in the home of my grandmother, my Aunt Gwendolin and my Uncle
+            Theodore Morgan.
+            
+            When I arrived this morning I was ushered into the sitting-room by a
+            maid, and the first one I beheld was my grandmother, sitting in a
+            rocking-chair. She called me to her, and crossing the room, I kotowed to
+            her, that is I went down on my hands and knees and touched my forehead
+            to the floor, as my Chinese nurse had taught me when I was yet a baby
+            that I should always do when I came into the presence of an elderly
+            woman, a mother of children.
+            
+            "My _dear_ grandchild!" cried my grandmother, "_do_ get up. All you
+            should do is to kiss me--your grandmother!" And she put out her hand and
+            assisted me from the floor.
+            
+            Grandmother is the dearest, prettiest little woman I ever saw, with
+            white hair and the brightest of eyes, and I have to love her, although I
+            had made up my mind to hate everything in America. A moment after she
+            had lifted me from the floor, my Aunt Gwendolin came in. She is tall and
+            thin, not nearly so beautiful a woman as my Chinese mother. She wears
+            skirts that drag on the floor, and her hair is built up into a sort of a
+            mountain on top of her head. I am reminded every time I look at her of a
+            certain peak in the Thian Shan mountains. I very much prefer little
+            women, like my own dear mother, like the women of my own country.
+            
+            My Uncle Theodore is long-armed, long-legged, long-bodied. He looks a
+            little like my father, and for that reason I hate him a little less than
+            my Aunt Gwendolin.
+            
+            After my mother's death, my father brought into our home a French
+            governess, daughter of a French consul, to teach me. Father seemed to
+            be lost in his business, or his grief at the loss of my mother, and paid
+            very little heed to me after the arrival of the governess.
+            
+            "She is an educated woman," he told me when he had engaged her, "and I
+            want her to teach you all you could learn in a first-class girls' school
+            in Europe or America."
+            
+            After that the French governess spent hours with me every day, and I saw
+            my father only at intervals. How much we talked about, that French lady
+            and I! Everything, almost, except religion; _that_ my father vetoed, as
+            her faith was not the one he wished me to embrace. "I'll take you over
+            to your grandmother by and by," he used to say, "to get the proper
+            religious instruction."
+            
+            The governess said that I inherited more from my father's side of the
+            house than my mother's; that although I was born in China, I was more
+            of an Occidental than an Oriental; more than once she said that my
+            American mannerisms and tricks of speech were really remarkable, and
+            that I was a living example of the power of heredity. But I am never
+            going back on my mother's people, _never_, my dear little oval-faced
+            mother whose grave is under a spreading camphor tree at the heart of the
+            world.
+            
+            Does it not mean something that China is at the centre of the world--the
+            kernel?
+            
+            "The girl is not bad to look at, in fact I think she is a beauty--a face
+            filled with the indescribable dash of the Orient," said my Uncle
+            Theodore, when they were talking me over in the sitting-room after I had
+            retired to my chamber upstairs. Evidently they had forgotten the
+            opening in the floor which had been left by the workmen while making
+            some changes in the plumbing. And they did not know my extraordinary
+            keenness of hearing, which my governess said was an Oriental trait.
+            
+            It seemed to give my governess some pleasure to talk about that keen
+            sense of the Orientals, and to speculate as to how they had acquired it.
+            "They have lived in a country where it is necessary, for
+            self-protection, to hear all that is being plotted and planned," she
+            said, "a country of conspiracies and intrigues, of plots and
+            counterplots. Centuries of this have developed abnormal hearing."
+            
+            "She has a superb figure," said my uncle, continuing to talk about me,
+            "and that oval face of hers, with her creamy complexion, is really
+            bewitching."
+            
+            "Yellow! you mean, _yellow_!" interrupted my Aunt Gwendolin; "she's
+            entirely too yellow for beauty. I'm terribly afraid that some of our set
+            will discover her nationality. That's _one_ thing you must remember,
+            Theodore, nobody on this continent is ever to learn anything about her
+            Chinese blood. They are so despised here as a race. She is our brother's
+            daughter, with some foreign strain inherited from her mother; that
+            is enough; never, _never_, let us acknowledge the Chinese. The
+            Italians and Spanish are yellowish too,--I have it!" she exclaimed,
+            "_Spanish!_--Spanish will do!--Some of those are _our_ people now, you
+            know! It will be quite interesting to have her a native of one of our
+            Dependencies--a descendant of some old Spanish family!"
+            </p>
+        );
+    }
+}
+
+export default Summary;
--- src/containers/dashboard/HealthRecord.js
+++ src/containers/dashboard/HealthRecord.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import tocbot from 'tocbot';
+
+import Summary from '../../components/dashboard/health-record/Summary';
 
 export class HealthRecord extends Component {
+
+  componentDidMount() {
+    tocbot.init({
+      tocSelector: '.js-toc',           // where to render the table of contents
+      contentSelector: '.js-toc-content',   // where to grab the headings to build the table of contents
+      headingSelector: 'h1'              // which headings to grab inside of the contentSelector element
+    });
+  }
+
   render() {
     return (
-      <div className="health-record"></div>
+      <div className='health-record'>
+        <div className='health-record__body'>
+          <div className='js-toc'></div>
+          <div className='js-toc-content'>
+            <h1 id='summary'>Summary </h1>
+            <Summary />
+            <h1 id='procedures'>Procedures</h1>
+            <p> Insert procedures component here.</p>
+            <h1 id='conditions'>Conditions</h1>
+            <p> Insert conditions component here.</p>
+            <h1 id='labs'>Labs</h1>
+            <p> Insert labs component here.</p>
+            <h1 id='medications'>Medications</h1>
+            <p> Insert medications component here.</p>
+            <h1 id='allergies'>Allergies</h1>
+            <p> Insert allergies component here.</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-
+    profile: state.profiles.currentProfile
   };
 }
 
