526440344f2b56d1ab29f55202f97c223bb9c790
sgober <sgober@mitre.org>
2018-07-18 14:56:24 -0400

    broken tests... need to be fixed

---
+++ src/__tests__/containers/dashboard/HealthRecord.test.js
@@ -0,0 +1,19 @@
+import { fullRenderComponent } from '../../../utils/testHelpers';
+import HealthRecord from '../../../containers/dashboard/HealthRecord';
+
+function setup() {
+    const store = {
+        auth: {
+          accessToken: 'abcd'
+        }
+    };
+    const props = {};
+    
+    return fullRenderComponent(HealthRecord);
+}
+
+it('renders self and self components', () => {
+    const component = setup();
+
+    expect(component).toBeDefined();
+});
