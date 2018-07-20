52596b93b249ae29e3c90fd41ad588eeb31fd2d4
sgober <sgober@mitre.org>
2018-07-20 11:43:44 -0400

    added and populated conditions table

--- src/components/dashboard/health-record/Conditions.js
+++ src/components/dashboard/health-record/Conditions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import VerticalTimeline from '../shared/VerticalTimeline';
 
@@ -13,8 +14,26 @@ export default class Conditions extends Component {
   render() {
     if (this.props.conditions.length === 0) return <div className="conditions no-entries">No entries.</div>;
 
+    const currentConditions = this.props.conditions.filter((condition) => 'active' === condition.clinicalStatus);
+	 added and populated conditions table
     return (
       <div className="conditions">
+        <div className="conditions__table-label">Current conditions list</div>
+        <div className="conditions__table">
+          <div className="conditions__table-row conditions__table-header">
+            <div className="conditions__table-condition"><span> condition</span></div>
+            <div className="conditions__table-diagnosed-date"><span> diagnosed date</span></div>
+            <div className="conditions__table-diagnosed-by"><span> diagnosed by</span></div>
+          </div>
+          {currentConditions.map((condition) => 
+            <div key={condition.id} className="conditions__table-row">
+              <div className="conditions__table-condition"> {condition.code.text}</div>
+              <div className="conditions__table-diagnosed-date"> {moment(condition.onsetDateTime).format('MMM D, YYYY')}</div>
+              <div className="conditions__table-diagnosed-by"></div>
+            </div>
+          )}
+        </div>
+
         <VerticalTimeline items={this.conditions()} icon="heartbeat" />
       </div>
     );
