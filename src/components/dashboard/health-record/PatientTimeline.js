3d93cc772ab0cd46c0ba244193b2d45758dd00ce
achakraborty <achakraborty@mitre.org>
2018-07-23 09:14:57 -0400

    latest changes made

--- src/components/dashboard/health-record/PatientTimeline.js
+++ src/components/dashboard/health-record/PatientTimeline.js
@@ -10,7 +10,7 @@ export default class PatientTimeline extends Component {
     super(props);
     const groups = [{ id: 1, title: 'procedure' }, {id: 2, title: 'condition'}, {id: 3, title: 'lab'}, {id: 4, title: 'medication'}];
     let groupHash = {}
-    groupHash['Procedure'] = ['fa fa-hospital-o', 1]
+    groupHash['Procedure'] = ['fa fa-hospital-o', 1, ]
     groupHash['Condition'] = ['fa fa-heartbeat', 2]
     groupHash['Lab'] = ['fa fa-flask', 3]
     groupHash['Medication'] = ['fa fa-stethoscope', 4]
