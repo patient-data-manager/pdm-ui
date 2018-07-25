f8ed7f029bfcdd390289564bd03a6168225963b7
Noranda Brown <nebrown@mitre.org>
2018-07-25 18:59:05 -0400

    Converted table to its own component and added to Conditions, added sorting

--- src/components/dashboard/health-record/Conditions.js
+++ src/components/dashboard/health-record/Conditions.js
@@ -3,52 +3,42 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 import VerticalTimeline from '../shared/VerticalTimeline';
+import TableList from '../shared/TableList';
 
 export default class Conditions extends Component {
-  conditions() {
+  getConditions() {
     return this.props.conditions.map((condition) => {
       return { date: condition.onsetDateTime, text: condition.code.text };
     });
   }
 
-  renderTable(conditions) {
-    if (conditions.length === 0) {
-      return <div className="conditions__table-label">No current conditions.</div>;
-    } else {
-      return (
-        <div className="conditions__table-container">
-          <div className="conditions__table-label">Current conditions list</div>
-          <div className="conditions__table">
-            <div className="conditions__table-header">
-              <div className="conditions__table-condition"><span> condition</span></div>
-              <div className="conditions__table-diagnosed-date"><span> diagnosed date</span></div>
-              <div className="conditions__table-diagnosed-by"><span> diagnosed by</span></div>
-            </div>
-            {conditions.map((condition) => 
-              <div key={condition.id} className="conditions__table-row">
-                <div className="conditions__table-condition"> {condition.code.text}</div>
-                <div className="conditions__table-diagnosed-date"> 
-                  {moment(condition.onsetDateTime).format('MMM D, YYYY')}
-                </div>
-                <div className="conditions__table-diagnosed-by"></div>
-              </div>
-            )}
-          </div>
-        </div>
-      );
-    }
+  getCurrentConditions() {
+    const currentConditions = this.props.conditions
+      .filter((condition) => condition.clinicalStatus === 'active')
+      .sort(((a, b) => moment(b.onsetDateTime) - moment(a.onsetDateTime)));
+
+    let filteredCurrentConditions = [];
+    currentConditions.forEach((condition, index) => {
+      filteredCurrentConditions[index] = {
+        condition: condition.code.text,
+        'diagnosed date': condition.onsetDateTime
+      };
+    });
+
+    return filteredCurrentConditions;
   }
 
   render() {
     if (this.props.conditions.length === 0) return <div className="conditions no-entries">No entries.</div>;
 
-    const currentConditions = this.props.conditions.filter((condition) => 'active' === condition.clinicalStatus)
-      .sort(((a, b) => moment(b.onsetDateTime) - moment(a.onsetDateTime)));
-
     return (
       <div className="conditions">
-        {this.renderTable(currentConditions)}
-        <VerticalTimeline items={this.conditions()} icon="heartbeat" />
+        <TableList
+          title="Current conditions list"
+          headers={['condition', 'diagnosed date']}
+          data={this.getCurrentConditions()}
+          formatters={{ 'diagnosed date': (value) => moment(value).format('MMM D, YYYY') }} />
+        <VerticalTimeline items={this.getConditions()} icon="heartbeat" />
       </div>
     );
   }
