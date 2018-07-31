a10638c41559709205ef8424b10fce29b055eeae
sgober <sgober@mitre.org>
2018-07-31 10:40:41 -0400

    additional check if coding is defined

--- src/utils/getDisplayString.js
+++ src/utils/getDisplayString.js
@@ -1,7 +1,9 @@
 export default function getDisplayString(object, field) {
   if (object[field] !== undefined) {
     if (object[field].text) return object[field].text;
-    if (object[field].coding[0].display) return object[field].coding[0].display;
+    if (object[field].coding && object[field].coding.length > 0) {
+      if (object[field].coding[0].display) return object[field].coding[0].display;
+    }
   }
   return '';
 }
