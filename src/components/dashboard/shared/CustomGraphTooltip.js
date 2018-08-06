9427fb9c1569242073518fa804206d88141d8f9b
sgober <sgober@mitre.org>
2018-08-06 14:08:12 -0400

    added date formatting

--- src/components/dashboard/shared/CustomGraphTooltip.js
+++ src/components/dashboard/shared/CustomGraphTooltip.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 export default class CustomGraphTooltip extends Component {
   render() {
     const { active, payload } = this.props;
     const details = payload.length > 0 ? payload[0].payload : null;
     if (active && details) {
+      const displayDate = moment(details.date).format('MMM D, YYYY');
       return (
         <div className="line-graph__tooltip">
           <div className="line-graph__tooltip-field"> 
-            <b>Date: </b> {details.date}
+            <b>Date: </b> {displayDate}
           </div>
           <div className="line-graph__tooltip-field"> 
             <b>{this.props.title}: </b> {details.value} {this.props.unit}
