94bc2c5aea2288fc0589ebc95dc91d21478597ba
sgober <sgober@mitre.org>
2018-08-07 09:53:47 -0400

    added custom ticks to x axis

--- src/components/dashboard/shared/LineGraph.js
+++ src/components/dashboard/shared/LineGraph.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import memoize from 'memoize-one';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ReferenceArea } from 'recharts';
+import { scaleTime } from 'd3-scale';
 
 import CustomGraphTooltip from './CustomGraphTooltip';
 
@@ -31,39 +32,41 @@ export default class LineGraph extends Component {
   }
 
   // memoize will only call the function if items has changed, otherwise it will return the last value
-  sortItemsByDate = memoize((items) => items.sort(((a, b) => moment(a.date) - moment(b.date))));
+  sortDataByDate = memoize((items) => items.sort(((a, b) => moment(a.date) - moment(b.date))));
+
+  processData = (data, xVar, xVarNumber) => {
+    let processedData = data;
+    data.forEach((entry, index) => {
+      processedData[index].date = moment(entry.date).valueOf();
+    });
+    return processedData;
+  }
 
   getMostRecentValue = (orderedData) => {
     const lastIndex = orderedData.length - 1;
     return orderedData[lastIndex].value;
   }
 
-  getMinMax = (data) => {
-    let rangeValues = [data[0].value, data[0].value];
+  getMinMax = (data, key) => {
+    let rangeValues = [data[0][key], data[0][key]];
     data.forEach((dataObj) => {
-      if (dataObj.value < rangeValues[0]) {
-        rangeValues[0] = dataObj.value;
-      } else if (dataObj.value > rangeValues[1]) {
-        rangeValues[1] = dataObj.value;
+      if (dataObj[key] < rangeValues[0]) {
+        rangeValues[0] = dataObj[key];
+      } else if (dataObj[key] > rangeValues[1]) {
+        rangeValues[1] = dataObj[key];
       }
     });
     return rangeValues;
   }
 
+  getTicks = (domain, numTicks) => {
+    const scale = scaleTime().domain(domain).range(domain);
+    return scale.ticks(numTicks);
+  }
+
   formatDate(dateString) {
     return moment(dateString).format(`MMM 'YY`);
   }
-
-  // processForGraphing = (data, xVar, xVarNumber) => {
-  //   // const dataCopy = Lang.clone(data);
-  //   const dataCopy = data;
-  //   console.log(dataCopy);
-
-  //   // Collection.map(dataCopy, (d) => {
-  //   //     d[xVarNumber] = Number(new Date(d[xVar]))
-  //   // });
-  //   return dataCopy;
-  // }
 
   resize = () => {
     if (!this.graphParentDiv) return;
@@ -138,31 +141,31 @@ export default class LineGraph extends Component {
   render() {
     if (this.props.data.length < this.props.minPoints) return;
 
-    const sortedData = this.sortItemsByDate(this.props.data);
-    // const processedData = this.processForGraphing(sortedData);
-    // console.log(processedData);
-
-    const [yMin, yMax] = this.getMinMax(sortedData);
+    const sortedData = this.sortDataByDate(this.props.data);
+    const processedData = this.processData(this.props.data);
+    const [xMin, xMax] = this.getMinMax(sortedData, 'date');
+    const [, yMax] = this.getMinMax(sortedData, 'value');
     const yUnit = '10^9/L'; // to-do add in unit
 
     return (
       <div className="line-graph" 
         ref={(graphParentDiv) => { this.graphParentDiv = graphParentDiv; }}>
-
-        {/* TO-DO: figure out dates */}
-        {/* TO-DO: add in tick marks */}
-
         <div className="line-graph__header">
           <div className="line-graph__header-title"> {this.props.title} </div>
           <div className="line-graph__header-most-recent"> 
             <span className="line-graph__field"> most recent: </span>
-            <span className="line-graph__value"> {this.getMostRecentValue(sortedData)}</span>
+            <span className="line-graph__value"> {this.getMostRecentValue(processedData)}</span>
           </div>
         </div>
-        <LineChart width={this.state.chartWidth} height={this.state.chartHeight} data={sortedData}>
+        <LineChart width={this.state.chartWidth} height={this.state.chartHeight} data={processedData}>
           <Line type="monotone" dataKey="value" stroke="#4a4a4a" />
           <Tooltip content={<CustomGraphTooltip title={this.props.title} unit={yUnit} />} />
-          <XAxis dataKey="date" tickFormatter={this.formatDate}/>
+          <XAxis 
+            dataKey="date" 
+            type="number"
+            domain={[xMin, xMax]}
+            ticks={this.getTicks([xMin, xMax], 4)}
+            tickFormatter={this.formatDate} />
           <YAxis dataKey="value" type="number" domain={[0, 'yMax']}/>
           {this.renderReferenceRanges(yMax)}
         </LineChart>
