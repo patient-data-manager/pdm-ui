19e2ef344eac6272c9d0011e2ce93b4194597165
Rob Dingwell <rob@robdingwell.org>
2018-08-10 15:47:33 -0400

    liniting

--- src/components/dashboard/health-record/Procedures.js
+++ src/components/dashboard/health-record/Procedures.js
@@ -5,17 +5,17 @@ import getDisplayString from '../../../utils/getDisplayString';
 import VerticalTimeline from '../shared/VerticalTimeline';
 
 export default class Procedures extends Component {
-
-  procedureDate(procedure){
+  procedureDate(procedure) {
     let date = procedure.performedDateTime;
-    if(!date && procedure.performedPeriod) {
-      date = procedure.performedPeriod.start
+    if (!date && procedure.performedPeriod) {
+      date = procedure.performedPeriod.start;
     }
     return date;
   }
+
   procedures() {
     return this.props.procedures.map((procedure) => {
-      const date = this.procedureDate(procedure)
+      const date = this.procedureDate(procedure);
       return { date, text: getDisplayString(procedure, 'code') };
     });
   }
