f73a239bf44db25190104b9b7e8917d2d6c18f42
Rob Dingwell <rob@robdingwell.org>
2018-08-10 17:11:50 -0400

    inital grouping of lab results by code to display in graphs

--- src/components/dashboard/health-record/Labs.js
+++ src/components/dashboard/health-record/Labs.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import getDisplayString from '../../../utils/getDisplayString';
-
+import getProperty from '../../../utils/getProperty';
 import LineGraph from '../shared/LineGraph';
 import VerticalTimeline from '../shared/VerticalTimeline';
 
@@ -39,9 +39,47 @@ export default class Labs extends Component {
     return (
       <div className="labs">
         <LineGraph title={"Hemoglobin (%)"} data={data} referenceRanges={referenceRanges} unit={'10^9/L'} />
+        {this.renderLabGraphs()}
         <VerticalTimeline items={this.labs()} icon="flask" />
       </div>
     );
+  }
+
+  renderLabGraphs() {
+    let groups = this.groupLabs();
+    let graphs = [];
+    for (var x in groups) {
+      let group = groups[x];
+      graphs.push(<LineGraph title={group.title}
+        data={group.values}
+        referenceRanges={group.referenceRanges}
+        unit={''} />);
+    }
+    return graphs.length > 0 ? graphs : '' ;
+  }
+
+  groupLabs() {
+    let grouped = {};
+    this.props.labs.forEach(function(lab) {
+      let code = getProperty(lab, "code.coding.firstObject.code");
+      let rr = getProperty(lab, "referenceRange");
+      let value = getProperty(lab, "valueQuantity");
+      if (code && code != "" && value) {
+        let group = grouped[code];
+        if (!group) {
+          group = { values: [],
+            title: getProperty(lab, "code.text") ||
+                           getProperty(lab, "code.coding.firstObject.display") };
+
+          grouped[code] = group;
+          if (rr) {
+            group['referenceRanges'] = rr.map(function(r) { return { high: r.high, low: r.low, assessment: r.text }; });
+          }
+        }
+        group.values.push({ value: value.value, date: lab.effectiveDateTime });
+      }
+    });
+    return grouped;
   }
 }
 
