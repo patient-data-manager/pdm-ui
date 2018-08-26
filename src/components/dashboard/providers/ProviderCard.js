f32c061b766cd56b2eefda3f03b67468e60df6f2
sgober <sgober@mitre.org>
2018-08-26 18:58:16 -0400

    initial provider titlebar styling

--- src/components/dashboard/providers/ProviderCard.js
+++ src/components/dashboard/providers/ProviderCard.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class ProfileCard extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  renderCollapseExpandIcon = () => {
+    return (
+      <FontAwesomeIcon icon="chevron-right" />
+    );
   }
 
   render() {
-    return <div> {this.props.provider.name} </div>;
+    return ( 
+      <div className="provider-card">
+        <div className="provider-card__titlebar">
+          <div className="provider-card__titlebar-name">
+            {this.props.provider.name}
+          </div>
+          <div className="provider-card__titlebar-icon">
+            {this.renderCollapseExpandIcon()}
+          </div>
+        </div>
+        <div className="provider-card__details"></div>
+      </div>
+    );
   }
 }
