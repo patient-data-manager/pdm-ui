991f677175499fb9bfcea7756fa16a23013c71e7
sgober <sgober@mitre.org>
2018-08-26 19:20:51 -0400

    added chevron toggle

--- src/components/dashboard/providers/ProviderCard.js
+++ src/components/dashboard/providers/ProviderCard.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class ProfileCard extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      detailsExpanded: false
+    };
+  }
+
+  toggleDetails = () => {
+    this.setState({ detailsExpanded: !this.state.detailsExpanded });
+  }
 
   renderCollapseExpandIcon = () => {
-    return (
-      <FontAwesomeIcon icon="chevron-right" />
-    );
+    if (this.state.detailsExpanded) {
+      return <FontAwesomeIcon icon="chevron-down" onClick={this.toggleDetails} />;
+    } else {
+      return <FontAwesomeIcon icon="chevron-right" onClick={this.toggleDetails} />;
+    }
   }
 
   render() {
     return ( 
       <div className="provider-card">
-        <div className="provider-card__titlebar">
+        <div className="provider-card__titlebar" role="button">
           <div className="provider-card__titlebar-name">
             {this.props.provider.name}
           </div>
@@ -28,3 +39,7 @@ export default class ProfileCard extends Component {
     );
   }
 }
+
+ProfileCard.propTypes = {
+  provider: PropTypes.object.isRequired,
+};
