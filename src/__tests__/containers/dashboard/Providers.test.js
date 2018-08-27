e87b37f26120a84e6262fa884cfba488c8d0d924
sgober <sgober@mitre.org>
2018-08-27 08:21:52 -0400

    fixed and updated broken providers tests

--- src/__tests__/containers/dashboard/Providers.test.js
+++ src/__tests__/containers/dashboard/Providers.test.js
@@ -3,18 +3,18 @@ import Providers from '../../../containers/dashboard/Providers';
 import { profileMockA } from '../../../__mocks__/profileMocks';
 import * as mocks from '../../../__mocks__/providerMocks';
 
-function setup() {
+function setup(providers) {
   const store = {
     profiles: {
       activeProfile: profileMockA,
     },
     providers: {
-      providers: [mocks.providerMockA, mocks.providerMockB, mocks.providerMockC],
+      providers: providers,
     }
   };
 
   const props = {
-    providers: [mocks.providerMockA, mocks.providerMockB, mocks.providerMockC],
+    providers: providers,
     profile: mocks.profileMockA,
     linkProviders: jest.fn(),
     loadProviders: jest.fn(),
@@ -24,17 +24,29 @@ function setup() {
 }
 
 it('renders self and self components', () => {
-  const component = setup();
+  const providers = [mocks.providerMockA, mocks.providerMockB, mocks.providerMockC];
+  const component = setup(providers);
 
   expect(component).toBeDefined();
   expect(component.find('div.providers')).toExist();
 });
 
 it('renders all providers', () => {
-  const component = setup();
+  const providers = [mocks.providerMockA, mocks.providerMockB, mocks.providerMockC];
+  const component = setup(providers);
 
-  expect(component.find('div.provider')).toHaveLength(3);
-  expect(component.find('div.provider > button').at(0).text()).toEqual('Fitbit');
-  expect(component.find('div.provider > button').at(1).text()).toEqual('Massachusetts General Hospital');
-  expect(component.find('div.provider > button').at(2).text()).toEqual('Partners Health Care');
+  expect(component.find('div.providers-list')).toExist();
+  expect(component.find('div.no-entries')).toHaveLength(0);
+  expect(component.find('div.provider-card')).toHaveLength(3);
+  expect(component.find('div.provider-card__titlebar-name').at(0).text()).toEqual('Fitbit');
+  expect(component.find('div.provider-card__titlebar-name').at(1).text()).toEqual('Massachusetts General Hospital');
+  expect(component.find('div.provider-card__titlebar-name').at(2).text()).toEqual('Partners Health Care');
+});
+
+it('displays no entries message if no vital entries are present', () => {
+  const component = setup([]);
+
+  expect(component.find('div.providers-list')).toExist();
+  expect(component.find('div.no-entries')).toExist();
+  expect(component.find('div.provider-card')).toHaveLength(0);
 });
