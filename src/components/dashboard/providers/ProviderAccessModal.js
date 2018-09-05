48a294838eefd9524bc391858a641fb79eb5f4ef
sgober <sgober@mitre.org>
2018-09-05 11:30:08 -0400

    modal footer btns and styling

--- src/components/dashboard/providers/ProviderAccessModal.js
+++ src/components/dashboard/providers/ProviderAccessModal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
+import Button from '@material-ui/core/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class ProviderAccessModal extends Component {
@@ -13,17 +14,17 @@ export default class ProviderAccessModal extends Component {
       <Modal
         isOpen={this.props.isOpen}
         onRequestClose={this.props.closeModal}
-        className="provider-access-modal"
-        overlayClassName="provider-access-modal__overlay">
+        className="provider-modal"
+        overlayClassName="provider-modal__overlay">
 
-        <div className="provider-access-modal__header">
-          <div className="provider-access-modal__header-title">{this.props.title}</div>
-          <div className="provider-access-modal__header-icon"> 
+        <div className="provider-modal__header">
+          <div className="provider-modal__header-title">{this.props.title}</div>
+          <div className="provider-modal__header-icon"> 
             <FontAwesomeIcon icon="times" onClick={this.props.closeModal} />
           </div>
         </div>
 
-        <div className="provider-access-modal__body">
+        <div className="provider-modal__body">
           <div>I am a modal</div>
           <form>
             <input />
@@ -33,8 +34,26 @@ export default class ProviderAccessModal extends Component {
             <button>the modal</button>
           </form>
         </div>
-        
-        <div className="provider-access-modal__footer"></div>
+
+        <div className="provider-modal__footer">
+          <div className="provider-modal__footer-buttons">
+            <Button
+              variant="outlined"
+              onClick={this.props.closeModal}
+              className="provider_modal__footer-button button-cancel">
+              CANCEL
+            </Button>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.props.closeModal}
+              // type="submit"
+              className="provider_modal__footer-button button-save">
+              SAVE
+            </Button>
+          </div>
+        </div>
       </Modal>
     );
   }
