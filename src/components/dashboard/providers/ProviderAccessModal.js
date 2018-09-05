581adb7a6e06249971c21eb1e2a96025f6363a93
sgober <sgober@mitre.org>
2018-09-05 12:41:56 -0400

    added partial permissions radio buttons and styling. added section rows and styling

--- src/components/dashboard/providers/ProviderAccessModal.js
+++ src/components/dashboard/providers/ProviderAccessModal.js
@@ -2,11 +2,76 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class ProviderAccessModal extends Component {
+  constructor(props) {
+    super(props);
+
+    let state = { 
+      accessLevel: 'none',
+      sectionAccess: this.getSections()
+    };
+    this.state = state;
+  }
+
   componentWillMount() {
     Modal.setAppElement('body');
+  }
+
+  getSections = () => {
+    return [
+      { title: 'Summary', access: 'none' }, 
+      { title: 'Conditions', access: 'none' }, 
+      { title: 'Allergies', access: 'none' }, 
+      { title: 'Medications', access: 'none' }, 
+      { title: 'Immunizations', access: 'none' }, 
+      { title: 'Proceedures', access: 'none' }, 
+      { title: 'Labs', access: 'none' },
+      { title: 'Vitals', access: 'none' }
+    ];
+  }
+
+  handleAccessChange = (event) => {
+    // TO-DO: change access levels in DB not just state
+    this.setState({ accessLevel: event.target.value });
+  }
+
+  renderRadioButton = (value, label) => {
+    return (
+      <FormControlLabel
+        className="access-radio"
+        control={
+          <Radio
+            checked={this.state.accessLevel === value}
+            onChange={this.handleAccessChange}
+            value={value}
+            name="access"
+            aria-label={value} />
+        }
+        label={label} />
+    );
+  }
+
+  renderPartialPermissionRow = (section, index) => {
+    return (
+      <div className="permissions-partial__section" key={index}>
+        <div className="permissions-partial__section-title">{section.title} </div>
+        <div className="permissions-partial__section-access">{section.access} </div>
+      </div>
+    );
+  }
+
+  renderPartialPermissions = () => {
+    if (this.state.accessLevel !== 'partial') return null;
+
+    return (
+      <div className="permissions-partial">
+        {this.state.sectionAccess.map((section, index) => this.renderPartialPermissionRow(section, index))}
+      </div>
+    );
   }
 
   render() {
@@ -25,14 +90,13 @@ export default class ProviderAccessModal extends Component {
         </div>
 
         <div className="provider-modal__body">
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+          <div className="permissions-title">Custom Permissions</div>
+          <div className="permissions-buttons">
+            {this.renderRadioButton('none', 'No access')}
+            {this.renderRadioButton('full', 'Full access (includes full view and edit access)')}
+            {this.renderRadioButton('partial', 'Partial access')}
+          </div>
+          {this.renderPartialPermissions()}
         </div>
 
         <div className="provider-modal__footer">
@@ -43,7 +107,6 @@ export default class ProviderAccessModal extends Component {
               className="provider_modal__footer-button button-cancel">
               CANCEL
             </Button>
-
             <Button
               variant="contained"
               color="primary"
