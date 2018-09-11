b4b7d8dafe81c0fff368a167ea23af2db73c3ee2
sgober <sgober@mitre.org>
2018-09-11 10:36:26 -0400

    split blood pressure vital into two values. added blood pressure string helper

--- src/components/dashboard/health-record/Vitals.js
+++ src/components/dashboard/health-record/Vitals.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import isValid from '../../../utils/isValid';
+import getBloodPressureString from '../../../utils/getBloodPressureString';
 import getDisplayString from '../../../utils/getDisplayString';
 
 import VerticalTimeline from '../shared/VerticalTimeline';
 
 export default class Vitals extends Component {
   vitals = () => {
-    return this.props.vitals.map((vital) => {
-      return { date: vital.effectiveDateTime, text: this.vitalDescription(vital) };
+    let vitals = [];
+
+    this.props.vitals.forEach((vital) => {
+      if (getDisplayString(vital, 'code') === 'Blood Pressure') {
+        vitals.push({ date: vital.effectiveDateTime, text: getBloodPressureString(vital, 'Systolic Blood Pressure') });
+        vitals.push({ date: vital.effectiveDateTime, text: getBloodPressureString(vital, 'Diastolic Blood Pressure') });
+      } else {
+        vitals.push({ date: vital.effectiveDateTime, text: this.vitalDescription(vital) });
+      }
     });
+
+    return vitals;
   }
 
   vitalDescription = (vital) => {
     let text = getDisplayString(vital, 'code');
     if (isValid(vital.valueQuantity)) text = `${text} ${vital.valueQuantity.value} ${vital.valueQuantity.unit}`;
-    // else if (!isValid(vital.valueQuantity) && text === 'Blood Pressure') {
-    //   console.log(vital)
-    // }
     return text;
   }
 
---
+++ src/utils/getBloodPressureString.js
@@ -0,0 +1,20 @@
+import getDisplayString from './getDisplayString';
+import isValid from './isValid';
+
+export default function getBloodPressureString(vital, type) {
+  if (isValid(vital.component)) {    
+    const matchingBloodPressure = vital.component.filter((component) => type === getDisplayString(component, 'code'));
+    if (matchingBloodPressure.length > 0) {
+      const bloodPressure = matchingBloodPressure[0];
+      const text = getDisplayString(bloodPressure, 'code');
+      let value = '';
+      let unit = '';
+      if (isValid(bloodPressure.valueQuantity)) {
+        value = isValid(bloodPressure.valueQuantity.value) ? bloodPressure.valueQuantity.value : '';
+        unit = isValid(bloodPressure.valueQuantity.unit) ? bloodPressure.valueQuantity.unit : '';
+      }
+      return `${text} ${value} ${unit}`;
+    }
+  }
+  return '';
+}
