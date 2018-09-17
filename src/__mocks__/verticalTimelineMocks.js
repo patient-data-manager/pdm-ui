2b6c33c979ce930d5d7b4a5924be03df6924cdec
sgober <sgober@mitre.org>
2018-09-17 10:11:10 -0400

    updated mocks to reflect icon change & linting

--- src/__mocks__/verticalTimelineMocks.js
+++ src/__mocks__/verticalTimelineMocks.js
@@ -1,43 +1,53 @@
 const verticalListMock = [
   {
     'date': '2014-07-13T02:27:04.196Z',
-    'text': '5'
+    'text': '5',
+    'icon': 'circle'
   },
   {
     'date': '2010-07-13T02:27:04.196Z',
-    'text': '9'
+    'text': '9',
+    'icon': 'circle'
   },
   {
     'date': '2018-07-13T02:27:04.196Z',
-    'text': '1'
+    'text': '1',
+    'icon': 'circle'
   },
   {
     'date': '2016-07-13T02:27:04.196Z',
-    'text': '3'
+    'text': '3',
+    'icon': 'circle'
   },
   {
     'date': '2011-07-13T02:27:04.196Z',
-    'text': '8'
+    'text': '8',
+    'icon': 'circle'
   }, 
   {
     'date': '2015-07-13T02:27:04.196Z',
-    'text': '4'
+    'text': '4',
+    'icon': 'circle'
   },
   {
     'date': '2009-07-13T02:27:04.196Z',
-    'text': '10'
+    'text': '10',
+    'icon': 'circle'
   },
   {
     'date': '2013-07-13T02:27:04.196Z',
-    'text': '6'
+    'text': '6',
+    'icon': 'circle'
   },
   {
     'date': '2012-07-13T02:27:04.196Z',
-    'text': '7'
+    'text': '7',
+    'icon': 'circle'
   },   
   {
     'date': '2017-07-13T02:27:04.196Z',
-    'text': '2'
+    'text': '2',
+    'icon': 'circle'
   }
 ];
 
