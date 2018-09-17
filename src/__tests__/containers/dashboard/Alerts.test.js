42d388ad1f07371eec8f12738c993c4d05256081
sgober <sgober@mitre.org>
2018-09-17 12:30:17 -0400

    updated alerts tests to reflect changes

--- src/__tests__/containers/dashboard/Alerts.test.js
+++ src/__tests__/containers/dashboard/Alerts.test.js
@@ -14,3 +14,12 @@ it('renders self and self components', () => {
   expect(component).toBeDefined();
   expect(component.find('div.alerts')).toExist();
 });
+
+it('renders the alerts lists', () => {
+  const component = setup();
+
+  expect(component.find('div.alerts-list')).toHaveLength(2);
+  expect(component.find('div.alerts-list__title').at(0).text()).toEqual('Alerts (insert count here)');
+  expect(component.find('div.alerts-list__title').at(1).text()).toEqual('Recently Approved');
+  expect(component.find('div.vertical-timeline')).toHaveLength(1);
+});
